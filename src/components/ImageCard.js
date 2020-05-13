9e0b4c8f3e8257e5e945b9ed053867edfe12aa9c
Breyanna Vass <breyannavass@gmail.com>
2020-05-13 16:48:32 -0400

    getting information from the API

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,22 +1,25 @@
 import React from "react";
 
-const ImageCard = () => {
+const ImageCard = ({ image }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl-mb-2">
-          Photo by John Doe
+          Photo by {image.user}
         </div>
         <ul>
           <li>
-            <strong>Views: </strong>4000
+            <strong>Views: </strong>
+            {image.views}
           </li>
           <li>
-            <strong>Downloads: </strong>300
+            <strong>Downloads: </strong>
+            {image.downloads}
           </li>
           <li>
-            <strong>Likes: </strong>400
+            <strong>Likes: </strong>
+            {image.likes}
           </li>
         </ul>
       </div>
