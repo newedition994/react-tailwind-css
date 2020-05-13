9b32fbff39eef16a1b2ea799314156090509f719
Breyanna Vass <breyannavass@gmail.com>
2020-05-13 16:19:32 -0400

    made a tag for the image

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ function App() {
         <div className="font-bold text-purple-500 text-xl-mb-2">
           Photo by John Doe
         </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>4000
+          </li>
+          <li>
+            <strong>Downloads: </strong>300
+          </li>
+          <li>
+            <strong>Likes: </strong>400
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
       </div>
     </div>
   );
