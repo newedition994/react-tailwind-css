1bb3dcc277b012bacd9472e360765c0e1ab64ff2
Breyanna Vass <breyannavass@gmail.com>
2020-05-13 16:13:52 -0400

    added in some information about the image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl-mb-2">
+          Photo by John Doe
+        </div>
+      </div>
     </div>
   );
 }
